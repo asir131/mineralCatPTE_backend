5174ea8e7b43cfa9b889d125dc1c8a5036cd9e24
infojihadsheikh-a11y <info.jihadsheikh@gmail.com>
2025-09-11 22:24:09 +0600

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
     'http://localhost:3002',
-    'https://minarelcat-admin.surge.sh',
-    'http://minarelcat-admin.surge.sh'
+    'http://209.142.65.188:3000'
 ];
 
 app.use(cors({
